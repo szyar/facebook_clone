bde7b95865cb5aac689308f5e2e073e95a33f2a4
szyar <soezayarszyar@gmail.com>
2020-08-10 11:54:16 +0630

    add new post functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,22 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
   def show
     @post = Post.find(params[:id])
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(params.require(:post).permit(:content))
+    if @post.save
+      flash[:notice] = "Post Created"
+      redirect_to @post
+    else
+      render 'new'
+    end
   end
 end
