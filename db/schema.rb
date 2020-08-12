6a2bdb04f72847a6eea3317774571cd71d411738
szyar <soezayarszyar@gmail.com>
2020-08-12 11:10:33 +0630

    connect user and posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_12_035059) do
+ActiveRecord::Schema.define(version: 2020_08_12_043121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_08_12_035059) do
     t.text "content"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
