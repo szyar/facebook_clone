566d4bace147cfaec0253fd23ef0b0bf14de9323
szyar <soezayarszyar@gmail.com>
2020-08-13 11:59:45 +0630

    add user profile functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,11 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
+  def show
+    @user = User.find(params[:id])
+    @posts = @user.posts
+  end
+
   def new
     @user = User.new
   end
