20a7c1663fc12c170b27a481269f19b8ffb5544e
szyar <soezayarszyar@gmail.com>
2020-08-13 11:13:16 +0630

    complete sign up functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,20 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:notice] = "Welcome #{@user.username}, you have successfully signed up"
+      redirect_to posts_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
 end
