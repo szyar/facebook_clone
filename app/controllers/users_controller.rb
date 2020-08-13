c9240f30742c320ddb3918599832db4917a9c528
szyar <soezayarszyar@gmail.com>
2020-08-13 11:29:19 +0630

    edit user functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,10 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -19,6 +23,16 @@ class UsersController < ApplicationController
       redirect_to posts_path
     else
       render 'new'
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Your account information was successfully updated"
+      redirect_to posts_path
+    else
+      render 'edit'
     end
   end
 
