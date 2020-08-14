692de9b41d69ae0c6d85ea62ff3d28810369ef05
szyar <soezayarszyar@gmail.com>
2020-08-14 10:36:29 +0630

    add delete user functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
-  before_action :require_user
-  before_action :require_same_user, only: [:edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, except: [:new, :create]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def page_title
     "Facebook"
@@ -40,6 +40,13 @@ class UsersController < ApplicationController
     else
       render 'edit'
     end
+  end
+
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+    flash[:notice] = "Account and all associated posts successfully deleted"
+    redirect_to root_path
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :posts
+  has_many :posts, dependent: :destroy
   before_save { self.email = email.downcase }
   validates :username, presence: true,
                       uniqueness: { case_sensitive: false },
