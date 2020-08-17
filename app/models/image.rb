3d9413375c977fec4d5451507333e5fddcabb6c2
szyar <soezayarszyar@gmail.com>
2020-08-17 11:12:59 +0630

    fix navbar

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,5 +1,5 @@
 class Image < ApplicationRecord
   belongs_to :user
   mount_uploader :picture, PictureUploader
-  validates :name, presence: true, length: { maximum: 150 }
+  validates :name, presence: true, length: { maximum: 63206 }
 end
